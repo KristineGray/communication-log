c104bcc392cffb1514e381ed257ef66e5bed240e
KristineGray <krisgraya@gmail.com>
2021-06-22 12:28:16 -0400

    Changed 17 to yellow

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -14,7 +14,7 @@ namespace communication_log
             Console.WriteLine("Wonderful storm we're having over here!");
             Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Not much better over here");
-            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Yellow;
             Console.WriteLine("We'll have to wait for the weather to clear before launch then.");
             Console.WriteLine("Weather seems to be clearning up.");
             Console.ReadKey();
