8aa4e0f777864a1c71d8d7d0393a241e9f654d4f
KristineGray <krisgraya@gmail.com>
2021-06-22 12:37:38 -0400

    Changed 17 to magenta

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -14,7 +14,7 @@ namespace communication_log
             Console.WriteLine("Wonderful storm we're having over here!");
             Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Not much better over here");
-            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Magenta;
             Console.WriteLine("We'll have to wait for the weather to clear before launch then.");
             Console.WriteLine("Weather seems to be clearning up.");
             Console.ReadKey();
