7a3a893bcabf532687a2d379e95980438ef619ce
KristineGray <krisgraya@gmail.com>
2021-06-22 12:17:58 -0400

    changed color to green and added line about weather

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -6,12 +6,13 @@ namespace communication_log
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Green;
             Console.WriteLine("Hello Pilot!");
             Console.WriteLine("Hello! Happy Tuesday!");
             Console.WriteLine("Wonderful storm we're having over here!");
             Console.WriteLine("Not much better over here");
             Console.WriteLine("We'll have to wait for the weather to clear before launch then.");
+            Console.WriteLine("Weather seems to be clearning up.");
             Console.ReadKey();
         }
     }
