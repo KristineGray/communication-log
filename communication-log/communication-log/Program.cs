26eef26558d0315eec76eceaa55209a7baf0c761
KristineGray <krisgraya@gmail.com>
2021-06-22 12:00:11 -0400

    Added 5th line to code

--- communication-log/communication-log/Program.cs
+++ communication-log/communication-log/Program.cs
@@ -10,6 +10,7 @@ namespace communication_log
             Console.WriteLine("Hello! Happy Tuesday!");
             Console.WriteLine("Wonderful storm we're having over here!");
             Console.WriteLine("Not much better over here");
+            Console.WriteLine("We'll have to wait for the weather to clear before launch then.");
             Console.ReadKey();
         }
     }
